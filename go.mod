250882505329cdb547ea7c5adbff1f2901879d86
Abigail <abigailekene6@gmail.com>
2024-11-21 04:22:05 -0800

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Abigailtony/example
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var boy = 4
+
+const girl = 7
+
+func main() {
+	school := 5
+	fmt.Printf("the vaue of boy is %v while the type is %T\n\n ", boy, boy)
+	fmt.Printf("the vaue of girl is %v while the type is %T\n\n", girl, girl)
+	fmt.Printf("the vaue of school is %v while the type is %T\n\n", school, school)
+
+}
